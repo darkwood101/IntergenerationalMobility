61e42d3c73cde6a2d466fa3efd801c5a1764dbd4
Dimitrije Pavlov <dpavlov@college.harvard.edu>
2022-04-10 22:57:48 -0400

    Add comments, clean up

--- aamodel/agent.py
+++ aamodel/agent.py
@@ -7,19 +7,28 @@ class privilege(Enum):
     NOT_PRIVILEGED = 2
 
 class agent:
-    a: float
-    a_dist: Callable[[], float]
-    c: privilege
-    sigma: float
-    tau: float
-    phi_0: float
-    p_A: float
-    p_D: float
+    # These are provided at construction time
+    a_dist: Callable[[], float]     # Distribution of abilities
+    c: privilege                    # Privilege (circumstance from the paper)
+    sigma: float                    # Ability multiplier
+    tau: float                      # Privilege multiplier
+    phi_0: float                    # Fraction of unprivileged agents in the
+                                    # generation to which this agent belongs
+    p_A: float                      # Probability of movement for privileged
+                                    # agents
+    p_D: float                      # Probability of movement for unprivileged
+                                    # agents
 
-    maybe_given: bool
-    given: bool
-    succeeded: bool
-    success_prob: float
+    # These are derived during initialization
+    a: float                        # Ability
+    maybe_given: bool               # `True` iff this agent was already
+                                    # considered for opportunity allocation
+    given: bool                     # `True` iff this agent was allocated an
+                                    # opportunity
+    succeeded: bool                 # `True` iff this agent was allocated an
+                                    # opportunity AND succeeded
+    success_prob: float             # Success probability for this agent
+
 
     # TODO: set default values of parameters
     def __init__(self,
@@ -49,9 +58,11 @@ class agent:
         self.given = False
         self.succeeded = False
 
-    # Returns true if this agent is privileged
+
+    # Returns true iff this agent is privileged.
     def is_privileged(self) -> bool:
         return self.c == privilege.PRIVILEGED
+
 
     # Gives an opportunity to this agent if success probability is above
     # threshold. Returns `True` if the opportunity is given and the agent
@@ -62,7 +73,8 @@ class agent:
         # Mark that this agent was considered for an opportunity
         self.maybe_given = True
 
-        # See if the agent exceeds the threshold and succeeds
+        # See if the agent exceeds the threshold (i.e. gets the opportunity)
+        # and succeeds
         self.given = self.success_prob >= threshold
         self.succeeded = self.given and random() <= self.success_prob
         
@@ -72,12 +84,13 @@ class agent:
 
         return self.succeeded
 
-    # Returns the offspring of this agent, computed according to the model
-    # Can only be called after opportunity allocation
-    def produce_offspring(self) -> 'agent':
-        assert self.maybe_given, "Cannot produce offspring until maybe given"
 
-        new_c = privilege
+    # Returns the offspring of this agent, computed according to the model.
+    # Can only be called after opportunity allocation.
+    def produce_offspring(self) -> 'agent':
+        assert self.maybe_given, "Cannot produce offspring maybe given"
+
+        new_c: privilege
         if self.is_privileged():
             new_c = privilege.NOT_PRIVILEGED \
                     if (random() <= self.p_A) and (random() <= self.phi_0) \
@@ -87,6 +100,7 @@ class agent:
                     if (random() <= self.p_D) and (random() > self.phi_0) \
                     else privilege.NOT_PRIVILEGED
 
+        # Everything is inherited from this agent, except possibly circumstance
         return agent(a_dist = self.a_dist,
                      c = new_c,
                      sigma = self.sigma,
@@ -95,8 +109,10 @@ class agent:
                      p_A = self.p_A,
                      p_D = self.p_D)
 
+
+    # Evolves the agent according to the model.
     # Considers giving the opportunity to this agent. Returns the agent's
-    # offspring, and a bool indicating whether the agent succeeded
+    # offspring, and a bool indicating whether the agent succeeded.
     def step(self, theta_0, theta_1) -> Tuple['agent', bool]:
         succeeded = self.maybe_give_opportunity(theta_0, theta_1)
         new_agent = self.produce_offspring()
--- aamodel/generation.py
+++ aamodel/generation.py
@@ -3,35 +3,48 @@ from typing import List, Callable, Tuple
 from random import random, shuffle
 
 class generation:
-    agents: List[agent]
-    N: int
-    sigma: float
-    tau: float
-    p_A: float
-    p_D: float
-    phi_0: float
-    a_dist: Callable[[], float]
+    # These are provided at construction time
+    # They are used for _all_ agents in the generation
+    a_dist: Callable[[], float]     # Distribution of abilities
+    sigma: float                    # Ability multiplier
+    tau: float                      # Privilege multiplier
+    phi_0: float                    # Fraction of unprivileged agents in this
+                                    # generation
+    p_A: float                      # Probability of movement for privileged
+                                    # agents
+    p_D: float                      # Probability of movement for unprivileged
+                                    # agents
+    N: int                          # Number of agents in this generation
+    agents: List[agent]             # Optional: List of agents from the previous
+                                    # generation, used to construct the next
+                                    # generation
 
-    n_successes: float
-    payoff: float
+    # These are derived during initialization
+    n_successes: float              # Number of successful agents in this
+                                    # generation
+    payoff: float                   # Undiscounted payoff from this generation
+
 
     def __init__(self,
-                 N: int,
+                 a_dist: Callable[[], float],
                  sigma: float,
                  tau: float,
                  phi_0: float,
                  p_A: float,
                  p_D: float,
-                 a_dist: Callable[[], float],
+                 N: int,
                  agents: List[agent]):
-        self.N = N
+        self.a_dist = a_dist
         self.sigma = sigma
         self.tau = tau
         self.p_A = p_A
         self.p_D = p_D
-        self.a_dist = a_dist
+        self.N = N
 
         if agents:
+            # In this case, we are constructing from the previous generation.
+            # It is the caller's responsibility to ensure that `phi_0` and
+            # `agents` are consistent with each other.
             self.agents = agents
             self.phi_0 = phi_0
         else:
@@ -67,10 +80,18 @@ class generation:
         self.n_successes = 0
         self.payoff = 0
 
-
-    def step(self, theta_0, theta_1) -> Tuple['generation', int]:
+    
+    # Evolves the generation according to the model.
+    # Allocates opportunities to agents in accordance with the provided
+    # thresholds `theta_0` and `theta_1`. Returns the next generation, and the
+    # undiscounted payoff from this generation.
+    def step(self, theta_0, theta_1) -> Tuple['generation', float]:
         new_agents: List[agent] = []
+        # Need to count the number of privileged agents for the new `phi_0`
         n_privileged = 0
+
+        # Update statistics by iterating through old agents, and construct
+        # the new agent list
         for a in self.agents:
             new_agent, succeeded = a.step(theta_0, theta_1)
             self.n_successes += int(succeeded)
@@ -78,6 +99,7 @@ class generation:
             new_agents.append(new_agent)
         new_phi_0 = 1 - n_privileged / self.N
 
+        # Construct the new generation
         new_generation = generation(N = self.N,
                                     sigma = self.sigma,
                                     tau = self.tau,
@@ -86,5 +108,6 @@ class generation:
                                     p_D = self.p_D,
                                     a_dist = self.a_dist,
                                     agents = new_agents)
-        payoff = self.n_successes / len(self.agents)
-        return new_generation, payoff
+        self.payoff = self.n_successes / len(self.agents)
+        return new_generation, self.payoff
+
